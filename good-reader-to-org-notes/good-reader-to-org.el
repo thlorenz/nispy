1b08b3130248d2fceebb7ef098008070ae3a3f09
Thorsten Lorenz <thlorenz@gmx.de>
2020-05-23 20:06:06 -0600

    good-reader: pulling out headers from lines

--- good-reader-to-org-notes/good-reader-to-org.el
+++ good-reader-to-org-notes/good-reader-to-org.el
@@ -5,19 +5,36 @@
 ;;; Commentary:
 ;;; Code:
 
-(defun is-string-empty (s)
-  "Detect if S is an emtpy string."
-  (string= "" s))
-
-(let ((annotations "
-line1
-
-line2
-
-line3
-"))
-  (seq-filter (lambda (s) (not (is-string-empty s)))
+(defun non-empty-lines (annotations)
+  "Pull out non-empty ANNOTATIONS lines."
+  (seq-filter (lambda (s) (not (string-empty-p s)))
               (split-string annotations "\n")))
+
+(non-empty-lines "
+  Highlight:
+  Practical: A Simple Database")
+
+(defun is-header-line (line)
+  "Determine if LINE is a header line."
+  (if (string-match "^Highlight\:" line) t nil))
+
+(let ((header nil))
+  (when header (pp "header")))
+
+(setq lines '("Highlight:" "Practical: A Simple Database"))
+
+;; TODO: dolist may work better here
+(defun parse-annotation-lines (list)
+  "Pull out header and item information from LIST."
+  (let ((acc) (header-is-next nil) (item-is-next nil) (current-page))
+    (while list
+      (when header-is-next (push (car list) acc))
+      (when (is-header-line (car list)) (setq header-is-next t))
+      (setq list (cdr list)))
+    acc))
+
+(parse-annotation-lines lines)
+
 
 (provide 'good-reader-to-org)
 ;;; good-reader-to-org.el ends here
