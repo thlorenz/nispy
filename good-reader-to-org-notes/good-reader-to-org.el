f651c2e066e201d13e1bf609fcefbf652bb1a462
Thorsten Lorenz <thlorenz@gmx.de>
2020-05-24 16:33:43 -0600

    good-reader: parsing items

--- good-reader-to-org-notes/good-reader-to-org.el
+++ good-reader-to-org-notes/good-reader-to-org.el
@@ -1,15 +1,12 @@
 ;;; good-reader-to-org.el --- Converts Good Reader Annotations to Org Mode Notes -*- lexical-binding: t; -*-
 
-(defconstant +HEADER+ "header")
+(defconst *header+ "header")
+(defconst *item+ "item")
 
 (defun non-empty-lines (annotations)
   "Pull out non-empty ANNOTATIONS lines."
   (seq-filter (lambda (s) (not (string-empty-p s)))
               (split-string annotations "\n")))
-
-(non-empty-lines "
-  Highlight:
-  Practical: A Simple Database")
 
 (defun is-header-line (line)
   (if (string-match "^Highlight\:" line) t nil))
@@ -17,35 +14,60 @@
 (defun is-item-line (line)
   (if (string-match "^Underline\:" line) t nil))
 
-(let ((header nil))
-  (when header (pp "header")))
+(defun has-lisp-code (line)
+  (if (string-match "^[(]" line) t nil))
+(defun has-algo-style-code (line)
+  (if (string-match "(^[{}])" line) t nil))
+
+(defun has-code (line)
+  (or (has-lisp-code line)
+      (has-algo-style-code line)))
 
 (defun make-parse-entry (type data)
-  (list :type type :data data))
+  (list :type type
+        :data data
+        :is-code: (and (equal *item+ type) (has-code data))))
+
+(pp
+ (progn
+   (defun parse-annotation-lines (list)
+     "Pull out header and item information from LIST."
+     (let (acc (header-is-next nil) (item-is-next nil) (current-page))
+       (nreverse
+        (dolist
+            (element list acc)
+          (cond (header-is-next
+                 (push (make-parse-entry *header+ element) acc)
+                 (setq header-is-next nil))
+
+                (item-is-next
+                 (progn
+                   (push (make-parse-entry *item+ element) acc)
+                   (setq item-is-next nil)))
+
+                ((is-header-line element)
+                 (setq header-is-next t))
+
+                ((is-item-line element)
+                 (setq item-is-next t))
+
+                )))))
+   (parse-annotation-lines lines)))
 
 
-(progn
-  (setq lines '(
-                "Highlight:"
-                "Practical: A Simple Database"
-                "Highlight:"
-                "Other Practical: A Simple Database"
-                ))
-  (defun parse-annotation-lines (list)
-    "Pull out header and item information from LIST."
-    (let ((acc) (header-is-next nil) (item-is-next nil) (current-page))
-      (nreverse
-       (dolist
-           (element list acc)
-         (cond (header-is-next
-                (progn
-                  (push (make-parse-entry "header" element) acc)
-                  (setq header-is-next nil)))
-               ((is-header-line (car list))
-                (setq header-is-next t)))
-         ))))
-  (parse-annotation-lines lines))
+(setq lines (non-empty-lines "
+Annotation summary:
 
+--- Page 45 ---
+
+Highlight:
+Practical: A Simple Database
+
+
+--- Page 46 ---
+
+Underline:
+(property list, or plist"))
 
 
 (provide 'good-reader-to-org)
